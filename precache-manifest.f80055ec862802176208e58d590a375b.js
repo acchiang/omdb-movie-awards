cb3dca50151529a8856d13d7e28f8deb7fab508b
Allison Chiang <acchiang98@gmail.com>
2020-09-06 16:53:02 -0700

    Updates

---
+++ precache-manifest.f80055ec862802176208e58d590a375b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e17eaeb02f5c0bcb4e762eae41454a",
+    "url": "/omdb-movie-awards/index.html"
+  },
+  {
+    "revision": "6fa610dba93041c0a224",
+    "url": "/omdb-movie-awards/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2989d54a746ed04e8ffa",
+    "url": "/omdb-movie-awards/static/js/2.6617eb07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/omdb-movie-awards/static/js/2.6617eb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa610dba93041c0a224",
+    "url": "/omdb-movie-awards/static/js/main.166efc01.chunk.js"
+  },
+  {
+    "revision": "a8c525989331f1ed0d61",
+    "url": "/omdb-movie-awards/static/js/runtime-main.3693031d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/omdb-movie-awards/static/media/logo.5d5d9eef.svg"
+  }
+]);
