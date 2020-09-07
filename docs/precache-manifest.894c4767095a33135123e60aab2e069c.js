848cdf7ed96365cc7ba7d7d8253d62377f9cd77d
Allison Chiang <acchiang98@gmail.com>
2020-09-07 15:00:42 -0700

    revert public folder name, change build folder to docs

---
+++ docs/precache-manifest.894c4767095a33135123e60aab2e069c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5e35c376867f44878f90d5e76d2942",
+    "url": "/omdb-movie-awards/index.html"
+  },
+  {
+    "revision": "d3f0f3c2cf2aa345420a",
+    "url": "/omdb-movie-awards/static/css/main.e688b5f9.chunk.css"
+  },
+  {
+    "revision": "7a8b9f7ebd93983f3fa8",
+    "url": "/omdb-movie-awards/static/js/2.a9b01d3e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/omdb-movie-awards/static/js/2.a9b01d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f0f3c2cf2aa345420a",
+    "url": "/omdb-movie-awards/static/js/main.4a1f85f3.chunk.js"
+  },
+  {
+    "revision": "a8c525989331f1ed0d61",
+    "url": "/omdb-movie-awards/static/js/runtime-main.3693031d.js"
+  }
+]);
