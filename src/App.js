d29b981ee3f35bdd3ffcbd3e21db5cbf463408db
Allison Chiang <acchiang98@gmail.com>
2020-09-06 19:11:09 -0700

    add dummy table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar.react";
+import TableData from "./components/TableData.react";
 import { Paper, Typography } from "@material-ui/core";
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
-  const content = (
+  const searchContent = (
     <div className="Search">
       <Typography variant="h3" component="h3">
         The Shoppies
@@ -13,9 +14,11 @@ function App() {
       <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} />
     </div>
   );
+
   return (
     <div className="App">
-      <Paper elevation={3} children={content} />
+      <Paper elevation={3} children={searchContent} />
+      <Paper elevation={3} children={<TableData searchInput={searchInput} />} />
     </div>
   );
 }
