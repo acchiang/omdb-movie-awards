e97c1989e0032694401b9117ce0219a1f73ffe49
Allison Chiang <acchiang98@gmail.com>
2020-09-06 18:58:53 -0700

    make search bar component

---
+++ src/components/SearchBar.react.js
@@ -0,0 +1,31 @@
+import React, { useCallback } from "react";
+import "./SearchBar.css";
+import { TextField } from "@material-ui/core";
+
+function SearchBar(props) {
+  console.log(props);
+  console.log(props.searchInput);
+  const searchInput = props.searchInput;
+  const setSearchInput = props.setSearchInput;
+  const onSearch = useCallback(
+    (event) => {
+      setSearchInput(event.target.value);
+      //todo other actions
+    },
+    [setSearchInput]
+  );
+  return (
+    <div id="search-bar">
+      <TextField
+        id="outlined-basic"
+        label="Search movie title"
+        variant="outlined"
+        fullWidth={true}
+        value={searchInput}
+        onChange={onSearch}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
