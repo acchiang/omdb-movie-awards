c5432764428c859a527a73f0b6f0ef8a0e335f07
Allison Chiang <acchiang98@gmail.com>
2020-09-07 15:39:23 -0700

    remove SearchBar css, add class styles

--- src/components/SearchBar.react.js
+++ src/components/SearchBar.react.js
@@ -1,19 +1,27 @@
 import React, { useCallback } from "react";
-import "./SearchBar.css";
 import { TextField } from "@material-ui/core";
+
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles({
+  searchBar: {
+    width: "70%",
+    padding: 10,
+  },
+});
 
 function SearchBar(props) {
   const searchInput = props.searchInput;
   const setSearchInput = props.setSearchInput;
+  const classes = useStyles();
+
   const onSearch = useCallback(
     (event) => {
       setSearchInput(event.target.value);
-      //todo other actions
     },
     [setSearchInput]
   );
   return (
-    <div id="search-bar">
+    <div className={classes.searchBar}>
       <TextField
         id="outlined-basic"
         label="Search movie title"
